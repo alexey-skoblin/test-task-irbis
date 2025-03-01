afb3d7fad48a8ebe7b9fe4af4dc7f7cbed4bd0be
alexey-skoblin <alexey.skoblin@gmail.com>
2025-03-01 21:27:37 +0400

    Remove Link Update Entites

--- src/main/java/com/alexey/skoblin/test_task_irbis/repository/ResourceRepository.java
+++ src/main/java/com/alexey/skoblin/test_task_irbis/repository/ResourceRepository.java
@@ -1,9 +1,9 @@
 package com.alexey.skoblin.test_task_irbis.repository;
 
-import com.alexey.skoblin.test_task_irbis.entity.WebSource;
+import com.alexey.skoblin.test_task_irbis.entity.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface WebSourceRepository extends JpaRepository<WebSource, UUID> {
+public interface ResourceRepository extends JpaRepository<Resource, UUID> {
 }
--- src/main/java/com/alexey/skoblin/test_task_irbis/repository/RubricRepository.java
+++ src/main/java/com/alexey/skoblin/test_task_irbis/repository/RubricRepository.java
@@ -1,9 +1,9 @@
 package com.alexey.skoblin.test_task_irbis.repository;
 
-import com.alexey.skoblin.test_task_irbis.entity.WebNews;
+import com.alexey.skoblin.test_task_irbis.entity.Rubric;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface WebNewsRepository extends JpaRepository<WebNews, UUID> {
+public interface RubricRepository extends JpaRepository<Rubric, UUID> {
 }
